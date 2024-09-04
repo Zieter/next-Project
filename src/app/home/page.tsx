9bfec17fd7fe1ab0d4fa49a93663fc0d4d9f0f85
https://github.com/Zieter/React-project.git <chunchen0118@gmail.com>
2024-09-04 21:12:03 +0800

    Update page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
     return (<>
-        <p>HomePage</p>
+        <p>HomePage1</p>
         <Link className="border rounded px-2 py-1 bg-blue-500 text-white" href={`/products`}>去產品頁</Link>
     </>
     );
