83b3d280f9175469b222849d0fef8e9c58361b69
https://github.com/Zieter/React-project.git <chunchen0118@gmail.com>
2024-09-04 21:52:59 +0800

    45

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
-{
-    "env": {
-        "test": {
-            "presets": [
+module.exports = {
+    env: {
+        test: {
+            presets: [
                 "@babel/preset-env",
                 "@babel/preset-react",
                 "@babel/preset-typescript"
             ]
         }
     }
-}
+};
